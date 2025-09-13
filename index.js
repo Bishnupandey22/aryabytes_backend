706e165d861d77c549dc05b41f40d1b306dd93c1
Bishnu <bishnupandey8906@gmail.com>
2025-09-13 23:50:09 +0530

    Added cors

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const queryRoutes = require('./Routes/queryRoutes');
 const { connectDB } = require('./config/Databse');
 
+const cors = require("cors")
 
 
+app.use(cors())
 app.use(express.json());
 app.use('/api/queries', queryRoutes);
 
